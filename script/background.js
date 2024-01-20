7f9dbd00c321fac5d0dab80c9fc76309ca71cded
seunghyun0522 <cvtcvt007@naver.com>
2024-01-20 20:43:46 +0900

    change style & background

--- script/background.js
+++ script/background.js
@@ -5,12 +5,12 @@ const chose = backgroundImg[Math.floor(Math.random() * backgroundImg.length)];
 
 const source = document.createElement("source");
 const sourceVideo = background.appendChild(source);
-sourceVideo.setAttribute("src", `./images/background/${chose}.mp4`);
+sourceVideo.setAttribute("src", `./imgs/background/${chose}.mp4`);
 sourceVideo.setAttribute("type", "video/mp4");
 
 const figure = document.querySelector(".randomImg figure");
 
 const img = document.createElement("img");
-img.src = `./images/background/${chose}.jpg`;
+img.src = `./imgs/background/${chose}.jpg`;
 
 figure.appendChild(img);
